6a7398cb20ca3a69ca86c05156492b688ea97aaa
SarvarKhalimov <60043698+SarvarKh@users.noreply.github.com>
2020-12-17 16:13:33 +0500

    Add calculator RSpec that adds two numbers

---
+++ spec/calculator_spec.rb
@@ -0,0 +1,8 @@
+describe Calculator do
+  describe "#add" do
+    it "returns the sum of two numbers" do
+      calculator = Calculator.new
+      expect(calculator.add(5, 2)).to eql(7)
+    end
+  end
+end
